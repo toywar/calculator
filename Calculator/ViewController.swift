0529eea9ea5f42704e372736d0b48e6adcdf6b0e
Anechka <anechka@anechka-mac.local>
2016-02-06 12:48:31 +0300

    add cos, sin, pi buttons and operate for they; add dot button

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -26,28 +26,32 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operate(sender: UIButton) {
+        let m_pi = M_PI
         let operation = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
         switch operation {
-        case "×": performOperation() { $0 * $1 }
-        case "÷": performOperation() { $1 / $0 }
-        case "−": performOperation() { $1 - $0 }
-        case "+": performOperation() { $0 + $1 }
-        case "√": performOperation() { sqrt($0) }
+        case "×": performBinaryOperation() { $0 * $1 }
+        case "÷": performBinaryOperation() { $1 / $0 }
+        case "−": performBinaryOperation() { $1 - $0 }
+        case "+": performBinaryOperation() { $0 + $1 }
+        case "√": performUnaryOperation() { sqrt($0) }
+        case "sin": performUnaryOperation() { sin($0) }
+        case "cos": performUnaryOperation() { cos($0) }
+        case "π": displayValue = m_pi; enter()
         default: break
         }
     }
     
-    func performOperation(operation: (Double, Double) -> Double) {
+    func performBinaryOperation(operation: (Double, Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
             enter()
         }
     }
     
-    private func performOperation(operation: Double -> Double) {
+    func performUnaryOperation(operation: Double -> Double) {
         if operandStack.count >= 1 {
             displayValue = operation(operandStack.removeLast())
             enter()
@@ -60,6 +64,11 @@ class ViewController: UIViewController {
         userIsInTheMiddleOfTypingANumber = false
         operandStack.append(displayValue)
         print("operandStack = \(operandStack)")
+    }
+    
+    @IBAction func clear(sender: UIButton) {
+        display.text = "0"
+        operandStack.removeAll()
     }
     
     var displayValue: Double {
